28e0a8faecdd60e790b40b5885c4c77cc5bbfd6c
opi3636 <yoyozohar2010@gmail.com>
2024-11-29 13:26:18 +0200

    Made hypredit universal

--- Documents/scripts/dot_hypredit.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-kate /home/opi3636/.config/waybar/config.jsonc /home/opi3636/.config/waybar/style.css /home/opi3636/.config/hypr/hyprland.conf /home/opi3636/.config/hypr/binds.conf /home/opi3636/.config/hypr/App_Autostart.conf
---
+++ Documents/scripts/hypredit.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+kate $HOME/.config/waybar/config.jsonc $HOME/.config/waybar/style.css $HOME/.config/hypr/hyprland.conf $HOME/.config/hypr/binds.conf $HOME/.config/hypr/App_Autostart.conf $HOME/.config/hypr/env.conf
